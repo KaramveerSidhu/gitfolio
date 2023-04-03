0956018513865619843602aac4fbc33e016d11e5
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-03 06:06:51 +0530

    [feature] creating an Icon component

---
+++ app/components/ui/Icon/index.tsx
@@ -0,0 +1,60 @@
+import * as profile from "./profile";
+import * as common from "./common";
+
+type propTypes = {
+  url: string;
+  width?: string;
+  height?: string;
+  fill?: string;
+  stroke?: string;
+};
+
+const Icon = (props: propTypes) => {
+  const { url = "", width, height, fill, stroke } = props;
+
+  let iconPack;
+  const splitUrl = url.split("-");
+  const [path, iconName] = splitUrl;
+
+  switch (path) {
+    case "pr":
+      iconPack = profile;
+      break;
+    default:
+      iconPack = common;
+  }
+
+  const iconMeta = iconPack[iconName];
+  const h = height || iconMeta.height;
+  const w = width || iconMeta.width;
+  const fill1 = fill || iconMeta.fill;
+  const stroke1 = stroke || iconMeta.stroke;
+
+  const svg = iconMeta.svg(fill1, stroke1);
+
+  return (
+    <div style={{ height: h, width: w, position: "relative" }}>
+      <svg
+        viewBox={`0 0 ${iconMeta.viewBoxWidth || iconMeta.width} ${
+          iconMeta.viewBoxHeight || iconMeta.height
+        }`}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {svg}
+      </svg>
+    </div>
+  );
+};
+
+export default Icon;
