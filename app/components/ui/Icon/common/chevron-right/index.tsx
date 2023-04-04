3231fc0ff042fb422cd4205588196c7b5b6b7c10
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-04 06:33:32 +0530

    adding a chevron right icon

---
+++ app/components/ui/Icon/common/chevron-right/index.tsx
@@ -0,0 +1,12 @@
+const chevron_right = {
+  width: 24,
+  height: 24,
+  fill: "none",
+  stroke: "#3A474E",
+  viewBox: "0 0 24 24",
+  svg: (_: string, stroke: string) => (
+    <path d="M9 18L14.5 12.5L9 7" stroke={stroke} strokeWidth="2" />
+  ),
+};
+
+export { chevron_right };
--- app/components/ui/Icon/common/index.ts
+++ app/components/ui/Icon/common/index.ts
@@ -1,1 +1,1 @@
-
+export * from "./chevron-right";
