aae00d529fea2284d5575c505c53c3996b25ef3c
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-05 06:06:30 +0530

    adding About & Contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import Cover from "@components/home-page/cover";
+import About from "./components/home-page/about";
+import Contact from "./components/home-page/contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Cover />
+      <About />
+      <Contact />
     </main>
   );
 }
