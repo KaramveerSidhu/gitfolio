17234ba5b7b609a644b6053f0e7d0796dddf2422
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-05 06:08:08 +0530

    creating a Footer component

---
+++ app/components/shared/footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.madeBy}>Made with 💝 by Karamveer</div>
+    </div>
+  );
+};
+
+export default Footer;
