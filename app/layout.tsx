2a79816296d7445f78a262ed04a5c3796d602e59
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-06 06:44:49 +0530

    moving layout contents to separate components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import styles from "./layout.module.scss";
-import Icon from "@components/ui/Icon";
 import Link from "next/link";
-import { GITHUB, LINKEDIN } from "@constants";
 import { NAME, SITE_DESCRIPTION } from "@constants";
 import Footer from "@components/shared/footer";
+import Sidebar from "./components/shared/sidebar";
+import Navbar from "./components/shared/navbar";
 
 export const metadata = {
   title: NAME,
@@ -19,30 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav className={styles.navbar}>
-          <ul className={styles.list}>
-            <li className={styles.listItem}> About </li>
-            <li className={styles.listItem}> Experience </li>
-            <li className={styles.listItem}> Projects </li>
-            <li className={styles.listItem}> Contact </li>
-            <li className={`${styles.listItem} ${styles.lastListItem}`}>
-              <Link
-                href="/karamveer-sidhu-resume.pdf"
-                className={styles.resumeLink}
-              >
-                Resume
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.profilesNav}>
-          <Link href={GITHUB} target="_blank" rel="noopener noreferrer">
-            <Icon url="pr-github" />
-          </Link>
-          <Link href={LINKEDIN} target="_blank" rel="noopener noreferrer">
-            <Icon url="pr-linkedin" />
-          </Link>
-        </div>
+        <Navbar />
+        <Sidebar />
         {children}
         <Footer />
       </body>
