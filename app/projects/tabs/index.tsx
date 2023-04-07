983e3349895e53b542ebd93ced13707b7e5093f5
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-07 09:20:10 +0530

    creating a Tabs component

---
+++ app/projects/tabs/index.tsx
@@ -0,0 +1,33 @@
+import styles from "./Tabs.module.scss";
+import clsx from "clsx";
+
+type propTypes = {
+  activeTab: string;
+  onChange: Function;
+};
+
+const Tabs = (props: propTypes) => {
+  const { activeTab, onChange = () => undefined } = props;
+
+  const isPersonalActive = activeTab === "personal";
+
+  return (
+    <div className={styles.tabs}>
+      <div
+        className={clsx(styles.tab, !isPersonalActive && styles.active)}
+        onClick={() => onChange("contributed")}
+      >
+        Projects I contributed to
+      </div>
+      <div> | </div>
+      <div
+        className={clsx(styles.tab, isPersonalActive && styles.active)}
+        onClick={() => onChange("personal")}
+      >
+        Personal Projects
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
