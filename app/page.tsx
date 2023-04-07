0e44582338be8cda77182c0a8d887f7c5cf3d9cd
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-07 09:19:16 +0530

    adding <main> to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import styles from "./page.module.scss";
 import Cover from "@components/home-page/cover";
 import About from "./components/home-page/about";
 import Contact from "./components/home-page/contact";
@@ -8,10 +7,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
       <Cover />
       <About />
       <Contact />
-    </main>
+    </>
   );
 }
