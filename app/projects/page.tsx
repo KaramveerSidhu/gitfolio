5a307726bb09c60f383df4c7eb09946cb2330fbf
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-09 09:32:16 +0530

    adding the ProjectsDisplay component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,12 +3,17 @@
 import Tabs from "./tabs";
 import styles from "./Projects.module.scss";
 import { useState } from "react";
+import { PROJECTS_PERSONAL } from "@data";
+import ProjectsDisplay from "./projects-display";
 
 const Projects = () => {
   const [activeTab, setActiveTab] = useState("contributed");
+  const isPersonalActive = activeTab === "personal";
+
   return (
     <div className={styles.projects}>
       <Tabs activeTab={activeTab} onChange={setActiveTab} />
+      <ProjectsDisplay projects={isPersonalActive ? PROJECTS_PERSONAL : []} />
     </div>
   );
 };
