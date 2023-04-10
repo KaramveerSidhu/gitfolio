84a07b99522be9fb040d9374bccadbb2a2472e7e
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-10 08:00:15 +0530

    adding Navbar logo

--- app/components/shared/navbar/index.tsx
+++ app/components/shared/navbar/index.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Navbar.module.scss";
+import clsx from "clsx";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.list}>
         <li className={styles.listItem}>
+          <Link href="/" className={styles.link}>
+            <div className={styles.logo}>
+              <Image
+                src="/logo-white.png"
+                fill
+                alt="Karamveer Singh Sidhu logo"
+              />
+            </div>
+          </Link>
+        </li>
+        <li className={clsx(styles.listItem, styles.firstListItem)}>
           <a href="/#about" className={styles.link}>
             About
           </a>
@@ -20,7 +33,7 @@ const Navbar = () => {
             Contact
           </a>
         </li>
-        <li className={`${styles.listItem} ${styles.lastListItem}`}>
+        <li className={clsx(styles.listItem, styles.lastListItem)}>
           <Link
             href="/karamveer-sidhu-resume.pdf"
             className={styles.resumeLink}
