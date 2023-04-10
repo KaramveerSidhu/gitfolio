8325fc50a239a4cb60df5672260cd02c9407ed7a
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-10 08:04:16 +0530

    adding covid19TrackerData

---
+++ data/projects/personal/covid-19-tracker.ts
@@ -0,0 +1,12 @@
+export const covid19TrackerData = {
+  id: "covid-19-tracker",
+  title: "Covid-19 Tracker",
+  description:
+    "A web application that monitors the number of new cases, recoveries, and fatalities caused by the Covid-19 outbreak.",
+  content: ` - Developed a pixel-perfect 100% mobile responsive user interface using React, Material UI and custom CSS. \n - Utilized the react-leaflet library to produce a visually informative map that displays the impacted areas with the number of cases. \n - Used react-chartjs to generate informative graphs that exhibit time-based data.
+        `,
+  sourceCodeURL: "https://github.com/KaramveerSidhu/covid-19-tracker",
+  appURL: "https://trailcovid19.netlify.app/",
+  imgURL: "/covid-19-tracker.png",
+  skills: ["React.js", "leaflet.js", "chart.js", "Material UI"],
+};
