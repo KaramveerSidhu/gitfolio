c84b22ebdbd0508f10abf75b057e3ca67cf6db4f
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-10 08:04:00 +0530

    adding aINewsAppData

---
+++ data/projects/personal/ai-news-app.ts
@@ -0,0 +1,12 @@
+export const aINewsAppData = {
+  id: "ai-news-app",
+  title: "AI Based Voice Controlled News Fetching Web App",
+  description:
+    "A voice controlled App that fetches News for you. The user can ask for the latest news, news by category, news by term, news by source. The app fetches and displays the news from the News API. The user can ask the app to read the headlines, open any article to read and go back. The app automatically scrolls down the page while reading the headlines.",
+  content: ` - Developed mobile responsive user interface by using React, Material UI and custom CSS. \n - Learned and used Alan AI and Alan Studio for Natural Language Processing to add voice capabilities to the app. \n - Deployed the application 100% on cloud using Netlify and Github.
+      `,
+  sourceCodeURL: "https://github.com/KaramveerSidhu/ai-news-app",
+  appURL: "https://ainewsapp.netlify.app/",
+  imgURL: "/ai-news-app.jpg",
+  skills: ["React.js", "Alan AI", "Alan Studio", "Material UI"],
+};
