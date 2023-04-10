369b94a14d818b77d8401fbc7354e97e78de34fb
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-10 08:04:35 +0530

    updating faceDetectionAppData

--- data/projects/personal/face-detection-app.ts
+++ data/projects/personal/face-detection-app.ts
@@ -1,12 +1,12 @@
 export const faceDetectionAppData = {
-  id: "1",
+  id: "face-detection-app",
   title: "Face Detection On Cloud",
   description:
-    "A full stack application to detect face in an image on the cloud. This was a project I built while I was learning full stack development as a part of an Udemy Course.",
+    "A full stack application to detect face in an image on the cloud. This was a project I built while I was learning full stack development.",
   content: ` - Built a full stack JavaScript application to detect face in an image on the cloud with user registration.\n - Used React.js, HTML5, CSS3, React Bootstrap and particles.js on the front-end. Created a Restful API on the back-end using Node.js and Express.js to handle requests from the front-end and respond accordingly after interacting with a PostgreSQL database using knex.\n - Deployed the application 100% on cloud using Heroku, Git and Github.
     `,
-  sourceCodeURL: "https://facedetectionreact.herokuapp.com/",
-  appURL: "https://facedetectionreact.herokuapp.com/",
+  sourceCodeURL: "https://github.com/KaramveerSidhu/face-detection",
+  appURL: "https://facedetectionreact.herokuapp.com",
   imgURL: "/face-detection-app.jpg",
-  skills: ["React.js", "Node.js", "Heroku", "PostgreSQL"],
+  skills: ["React.js", "Node.js", "Heroku", "PostgreSQL", "Knex.js"],
 };
