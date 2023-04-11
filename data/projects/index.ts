f7a8a8cb9b5ceb68f6a0ca9193f9bae84c2e7676
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:30:51 +0530

    adding new projects

--- data/projects/index.ts
+++ data/projects/index.ts
@@ -1,12 +1,23 @@
 import {
+  bevigilData,
+  cloudsekPlatformData,
+  exposureAppData,
+} from "./contributed";
+import {
   aINewsAppData,
   covid19TrackerData,
   faceDetectionAppData,
+  faceRecognitionData,
 } from "./personal";
 
-export const PROJECTS_CONTRIBUTED = [{}];
+export const PROJECTS_CONTRIBUTED = [
+  cloudsekPlatformData,
+  bevigilData,
+  exposureAppData,
+];
 export const PROJECTS_PERSONAL = [
   covid19TrackerData,
   aINewsAppData,
   faceDetectionAppData,
+  faceRecognitionData,
 ];
