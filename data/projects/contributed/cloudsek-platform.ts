65b427b9c5d1d62c3308651a3d1100cfc2154656
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:31:23 +0530

    adding CloudSEK platform app data

---
+++ data/projects/contributed/cloudsek-platform.ts
@@ -0,0 +1,21 @@
+export const cloudsekPlatformData = {
+  id: "cloudsek-platform",
+  title: "CloudSEK Platform",
+  description:
+    "CloudSEK platform is a comprehensive cybersecurity software product that is designed to help organizations proactively monitor, identify and mitigate threats to their digital assets, including web applications, websites, and cloud infrastructure. The platform provides detailed reports & dashboards on various aspects of an organization's cybersecurity posture. It leverages AI-powered algorithms to detect and mitigate potential cyber threats before they can cause damage. The platform can detect a wide range of cyber threats, including malware, phishing, hacking attempts, and data breaches.",
+  content: ` - Created data visualization components, dashboards, new pages, did bug fixes & performance optimizations in the company's main user facing products, XVigil & SVigil (part of CloudSEK Platform). \n - Implemented a chunked upload solution, for uploading an apk file that reduced the upload time by 40 - 50% and bypassed the upload file size limit. \n - Quickly developed Exposure Search App in collaboration with other developers that led to 1lakh+ searches & 3000+ new leads for CloudSEK. \n - My contributions to CloudSEK platform helped to improve the product's user experience & functionality. My work on data visualization, dashboards, and bug fixes helped to make the product more effective and efficient for our users.
+          `,
+  sourceCodeURL: "",
+  appURL: "https://cloudsek.com/",
+  imgURL: "/cloudsek-platform.webp",
+  skills: [
+    "Next.js",
+    "React.js",
+    "Node.js",
+    "Nest.js",
+    "PostgreSQL",
+    "Docker",
+    "Redis",
+    "Recharts",
+  ],
+};
