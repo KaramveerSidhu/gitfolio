6b432219117c13ff906d5fcb4c955072142999a1
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:31:05 +0530

    adding bevigil data

---
+++ data/projects/contributed/bevigil.ts
@@ -0,0 +1,12 @@
+export const bevigilData = {
+  id: "cloudsek-bevigil",
+  title: "BeVigil (Security Search Engine)",
+  description:
+    "BeVigil is the internet's first and only security search engine for mobile apps. It enables users to identify potential vulnerabilities or secrets in apps by performing deep analysis through an android app scanner. This can help prevent security breaches and protect sensitive information. It provides users with a detailed report of its findings, allowing them to take the necessary steps to secure their applications.",
+  content: ` - Created highly performant, reusable, shared React components in Next.js for the revamp of world's first mobile security search engine BeVigil. \n - Reduced the MongoDB count query execution time by 88%. \n - Created dashboard pages for the admin panel and improved the existing ones by adding new search functionalities, features and more filters. Used Sequelize and React Admin. \n - Created and optimized the Docker images for both the frontend and backend services. \n - Created new APIs and improved existing ones written in Node.js. Cached computational count query results in Redis.
+        `,
+  sourceCodeURL: "",
+  appURL: "https://bevigil.com/",
+  imgURL: "/bevigil.png",
+  skills: ["React.js", "Node.js", "MongoDB", "PostgreSQL", "Docker", "Redis"],
+};
