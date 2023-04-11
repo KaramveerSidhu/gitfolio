3db9750e391528ddf9390229bfc320062aa8cc8f
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:32:34 +0530

    updating exports

--- data/projects/personal/index.ts
+++ data/projects/personal/index.ts
@@ -1,3 +1,4 @@
 export * from "./ai-news-app";
-export * from "./face-detection-app";
 export * from "./covid-19-tracker";
+export * from "./face-detection-app";
+export * from "./face-recognition";
