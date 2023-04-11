35613982d136c04998a44fd9c49953d2d11eb2bd
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:31:42 +0530

    adding cloudsek's exposure app data

---
+++ data/projects/contributed/exposure-app.ts
@@ -0,0 +1,12 @@
+export const exposureAppData = {
+  id: "cloudsek-exposure-app",
+  title: "CloudSEK's Exposure Search",
+  description:
+    "CloudSEK's Exposure Search can be used to check if data related to your organisation has been put up in past 90 days, for sale on marketplaces on the Dark/Deep Web.",
+  content: ` - As part of a collaborative effort with other developers, I played a key role in rapidly developing the Exposure Search App, which resulted in over 100,000 searches and generated more than 3,000 new leads for CloudSEK. \n - Our app provided users with a simple and effective way to identify potential vulnerabilities and exposures, which helped to increase awareness and interest in CloudSEK's cybersecurity solutions.
+            `,
+  sourceCodeURL: "",
+  appURL: "https://exposure.cloudsek.com",
+  imgURL: "/cloudsek-exposure.png",
+  skills: ["Next.js", "React.js"],
+};
