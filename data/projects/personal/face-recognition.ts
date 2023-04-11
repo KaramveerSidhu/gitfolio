1a1007e1c6966ab12ab5518d5a6f732450cb20ef
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-11 08:32:17 +0530

    adding faceRecognition project data

---
+++ data/projects/personal/face-recognition.ts
@@ -0,0 +1,17 @@
+export const faceRecognitionData = {
+  id: "face-recognition",
+  title: "Face Recognition",
+  description:
+    "A Face Recognition project in Python that can be employed for diverse applications.",
+  content: ` - Built a face recognition project to recognize faces in live feed.\n - Used Python, Jupyter Notebook & face_recognition library for the same.\n - In the first part of the project, we create face encodings by using the available images. These encodings are then saved in .dat files. \n - In the second part of the project, we execute the facial recognition process & a camera live feed window will appear. \n - The user can press 'r' on their keyboard to initiate the recognition process. The result of the recognition process will then be displayed. If the recognized user is a registered user, the program can be modified to take further action based on the user's identity.
+      `,
+  sourceCodeURL: "https://github.com/KaramveerSidhu/face-recognition",
+  appURL: "",
+  imgURL: "/face-recognition.gif",
+  skills: [
+    "Python",
+    "Jupyter Notebook",
+    "Machine Learning",
+    "face_recognition",
+  ],
+};
