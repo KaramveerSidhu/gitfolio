48c350287599916a28f8c690935a220637c1ac15
KaramveerSidhu <karamveersidhu14@gmail.com>
2023-04-13 07:16:05 +0530

    adding tutort project data

---
+++ data/projects/personal/tutort.ts
@@ -0,0 +1,12 @@
+export const tutortData = {
+  id: "tutort-academy",
+  title: "Tutort Academy Web Application",
+  description:
+    "Tutort Academy provides online courses for working professionals. I interned with them and created their web app independently from scratch.",
+  content: ` - Planned, designed, and developed the company's Web App by creating flexible, object oriented, reusable components in React using relevant Data Structures & Algorithms. \n - Completed the minimum project requirements well on time by prioritizing the important tasks. \n - Deployed the app on cloud using Ubuntu Linux EC2 instance of AWS. \n - Used Amazon Aurora PostgreSQL cloud database. Developed a backend server using Node.js. \n - Saved costs by installing free SSL certificate from Amazon Certificate Manager of AWS. \n - Improved the SEO by learning and using prerender service to respond differently to bots and users. - Received free enrollment into a course provided by the academy as an award for working up to the expectations.
+        `,
+  sourceCodeURL: "",
+  appURL: "https://tutort.net",
+  imgURL: "/tutort.png",
+  skills: ["React.js", "Node.js", "prerender", "PostgreSQL", "SCSS"],
+};
