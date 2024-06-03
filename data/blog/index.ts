c5098815845cf850bbd11f30fcf8704e4f8f0cea
KaramveerSidhu <karamveersidhu14@gmail.com>
2024-06-03 23:34:00 +0530

    feat: add a new blog

--- data/blog/index.ts
+++ data/blog/index.ts
@@ -1,26 +1,32 @@
 export const BLOGS = [
   {
-    title: "Exploring Bun: Setting up basic Server Side Rendering with React",
+    title: 'Top 5 famous software supply chain attacks in 2023',
     description:
-      "In this blog article, I explore the basics of Bun, the new JavaScript runtime, and set up basic Server Side Rendering with React on a Bun server.",
-    url: "https://medium.com/@karamsidhu/exploring-bun-setting-up-basic-server-side-rendering-with-react-95324ac6119d",
+      'Explore the critical nature of supply chain cyber attacks and learn how to fortify your defenses against this growing threat in 2023.',
+    url: 'https://www.cloudsek.com/blog/top-5-famous-software-supply-chain-cyber-attacks-in-2023',
   },
   {
-    title: "NextJs 13.4: First Look",
+    title: 'Exploring Bun: Setting up basic Server Side Rendering with React',
     description:
-      "This LinkedIn post highlights a quick summary of the new features of the app dir introduced in the latest version of Next.Js, 13.4 at that time.",
-    url: "https://www.linkedin.com/posts/activity-7060462179740758016-a69R?utm_source=share&utm_medium=member_desktop",
+      'In this blog article, I explore the basics of Bun, the new JavaScript runtime, and set up basic Server Side Rendering with React on a Bun server.',
+    url: 'https://medium.com/@karamsidhu/exploring-bun-setting-up-basic-server-side-rendering-with-react-95324ac6119d',
   },
   {
-    title: "NAN (Not a Number) in JavaScript",
+    title: 'NextJs 13.4: First Look',
     description:
-      "This short LinkedIn post highlights the intricasies associated with using NaN in JavaScript.",
-    url: "https://www.linkedin.com/posts/activity-7053941779380727808-He71?utm_source=share&utm_medium=member_desktop",
+      'This LinkedIn post highlights a quick summary of the new features of the app dir introduced in the latest version of Next.Js, 13.4 at that time.',
+    url: 'https://www.linkedin.com/posts/activity-7060462179740758016-a69R?utm_source=share&utm_medium=member_desktop',
   },
   {
-    title: "Do you know why software developers love coffee so much?",
+    title: 'NAN (Not a Number) in JavaScript',
     description:
-      "In this simple blog post, I emphasize the benefits of coffee and why we as developers love it.",
-    url: "https://www.linkedin.com/pulse/do-you-know-why-software-developers-love-coffee-so-much-singh-sidhu",
+      'This short LinkedIn post highlights the intricasies associated with using NaN in JavaScript.',
+    url: 'https://www.linkedin.com/posts/activity-7053941779380727808-He71?utm_source=share&utm_medium=member_desktop',
+  },
+  {
+    title: 'Do you know why software developers love coffee so much?',
+    description:
+      'In this simple blog post, I emphasize the benefits of coffee and why we as developers love it.',
+    url: 'https://www.linkedin.com/pulse/do-you-know-why-software-developers-love-coffee-so-much-singh-sidhu',
   },
 ];
